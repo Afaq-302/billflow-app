f0972488de0a71d9dd83143e4d95ee3e63d52aa2
Afaq-302 <ufaq302@yahoo.com>
2026-01-15 10:53:17 +0500

    error fix

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,9 +2,6 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 
-export const dynamic = "force-dynamic"; // force runtime evaluation per request
-export const revalidate = 0;
-export const runtime = "nodejs";        // important: next-auth v4 is not Edge-friendly
-
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
